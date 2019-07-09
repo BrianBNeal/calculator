76cffa9bc19e2604479461a3b33aebb7e8914894
Brian Neal <brianbneal@gmail.com>
2019-07-09 14:12:53 -0500

    can input digits and decimal

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+//example: 10 + 12
+//first operand is 10
+//operator is +
+//second operand is 12
+
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null
+    displayValue: '0', //string representing user input or result of operation
+    firstOperand: null, //first operand for any expression
+    waitingForSecondOperand: false, //a boolean to show if an expression should be evaluated
+    operator: null //the operator for an expression
 }
 
 const updateDisplay = () => {
@@ -10,7 +15,42 @@ const updateDisplay = () => {
     display.value = calculator.displayValue;
 }
 
-// event listener to add keypress to displayValue and then updateDisplay
-//one for digits, one for operators, one for decimal, one for clear
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const { target } = event;
 
-// function to handle operator to switch from first to second operand
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+})
+
+const inputDigit = (digit) => {
+    const {displayValue} = calculator;
+    calculator.displayValue = displayValue ==='0' ? digit : displayValue + digit;
+}
+
+const inputDecimal = (decimal) => {
+    const {displayValue} = calculator;
+    if (!displayValue.includes(decimal)) {
+        calculator.displayValue += decimal;
+    }
+}
